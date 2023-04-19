5dfdace452e24b440ff017a7d7b64822271821be
Martin Dobrev <mclueppers@users.noreply.github.com>
2023-04-19 19:02:05 +0100

    Remove port from api.js (#5)

--- src/src/api.js
+++ src/src/api.js
@@ -2,21 +2,15 @@
 import axios from "axios";
 
 // Set REQUEST_HOST based on the current domain
-const { protocol, host, port } = window.location;
-var reqhost;
-if (port) {
-  reqhost = `${host}:${port}`;
-} else {
-  reqhost = host;
-}
+const { protocol, host } = window.location;
 
-export const REQUEST_HOST = `${protocol}//${reqhost}`;
+export const REQUEST_HOST = `${protocol}//${host}`;
 
 export async function fetchPgpData() {
   // Check if host contains "localhost"
   // If so, use the local API server
   // Otherwise, use the production API server
-  const isLocalhost = reqhost.includes("localhost") || reqhost.includes("192.168.");
+  const isLocalhost = host.includes("localhost") || host.includes("192.168.");
   const apiHost = isLocalhost ? "http://keyserver.dobrev.it:11371" : REQUEST_HOST;
   const response = await axios.get(`${apiHost}/pks/lookup?op=stats&options=mr`);
   return response.data;
